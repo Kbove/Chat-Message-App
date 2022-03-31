0c4edbe64fb8875221c1aa3d96b7e271502c576a
Kbove <kbove94@gmail.com>
2022-03-31 14:35:15 -0700

    working out some things

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Homepage from './components/pages/homepage/Homepage'
 
 function App() {
 
     return (
         <Router>
-            <p1>hello</p1>
+            <Routes>
+                <Route path='/'>
+                    <Homepage/>
+                </Route>
+            </Routes>
         </Router>
     )
 }
---
+++ src/components/pages/homepage/Homepage.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+
+function Homepage() {
+
+    console.log('hello world')
+    return (
+        <>
+        <h1>Chat!</h1> 
+        </>
+    )
+}
+
+export default Homepage
