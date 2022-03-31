9bf3d393a7ee1cecdaf3edd2a455fc8edfd90c01
Kbove <kbove94@gmail.com>
2022-03-31 12:10:34 -0700

    hello world

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+function App() {
+
+    return (
+        <Router>
+            <p1>hello</p1>
+        </Router>
+    )
+}
+
+export default App;
